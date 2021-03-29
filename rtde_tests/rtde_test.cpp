3e6f1ebe937e506d41c12eab5b8bd2fecec47857
Johannes Buerst <buerst@finf.uni-hannover.de>
2021-03-29 16:18:07 +0200

    changes for testing rtde

--- rtde_tests/rtde_test.cpp
+++ rtde_tests/rtde_test.cpp
@@ -14,16 +14,30 @@ template <typename T> int sgn(T val) {
 int main(int argc, char *argv[])
 {
     float sT = 0.002; //sample Time
-    RTDEControlInterface rtde_control("127.0.0.1");
-    RTDEReceiveInterface rtde_receive("127.0.0.1");
+    RTDEControlInterface rtde_control("172.30.10.1");
+    RTDEReceiveInterface rtde_receive("172.30.10.1");
     std::vector<double> init_q = rtde_receive.getActualQ();
     int steps = 2000;
     int max_vel = 3	;
+    printf("Joint 0: %f\n",rtde_receive.getActualQ().at(0));
+    printf("Joint 1: %f\n",rtde_receive.getActualQ().at(1));
+    printf("Joint 2: %f\n",rtde_receive.getActualQ().at(2));
+    printf("Joint 3: %f\n",rtde_receive.getActualQ().at(3));
+    printf("Joint 4: %f\n",rtde_receive.getActualQ().at(4));
+    printf("Joint 5: %f\n",rtde_receive.getActualQ().at(5));
+
+    printf("TCP offset x: %f\n", rtde_receive.getActualTCPPose().at(0));
+    printf("TCP offset y: %f\n", rtde_receive.getActualTCPPose().at(1));
+    printf("TCP offset z: %f\n", rtde_receive.getActualTCPPose().at(2));
+    printf("TCP offset rx: %f\n", rtde_receive.getActualTCPPose().at(3));
+    printf("TCP offset ry: %f\n", rtde_receive.getActualTCPPose().at(4));
+    printf("TCP offset rz: %f\n", rtde_receive.getActualTCPPose().at(5));
+
     std::vector<double> startpos = {-1.57, -1.57, 1.57, 0, 0, 0};
     std::vector<double> joint_speed = {0.0, 0.0, 0.0, 0.0, 0.0, 0.0};
-    rtde_control.moveJ(startpos);
-    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
-    for (int j = 0; j < steps; j++)
+    //rtde_control.moveJ(startpos);
+    //std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    /*for (int j = 0; j < steps; j++)
 	{   
         if(j < 1000)
         {
@@ -41,7 +55,7 @@ int main(int argc, char *argv[])
 		{
 			std::this_thread::sleep_for(std::chrono::duration<double>(sT - t_duration.count()));
 		}		
-	}
+	}*/
 	rtde_control.speedStop();
     rtde_control.stopScript();
 	rtde_receive.disconnect();
